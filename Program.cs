285a3dbfbe3d6a2cb5f122568af001b75a7254ed
Gessielle <gessielle-barossi@hotmail.com>
2020-10-10 16:08:03 -0300

    Hora e data

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Usuário;
+            
+            DateTime horarioAtualDoSistema;
+
+            horarioAtualDoSistema = DateTime.Now;
+
+            Console.Write("Digite seu nome:");
+            Usuário = Console.ReadLine();
+
+            Console.WriteLine($"Olá {Usuário}\nA Sua Data e Horário Atual é!: \n{horarioAtualDoSistema}");
+        
+            
+
+
+            
+        }
+    }
+}
